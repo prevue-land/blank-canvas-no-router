2df16f48efcf7754af76d0d1ec0de0be3997117b
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-26 14:52:14 +0100

    Blank canvas, but without the router

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+  <main>
+    <h1>blank-canvas-no-router</h1>
+    <span>
+      &copy; {{ currentYear }} Weekly OverVue. Released under MIT License
+    </span>
+  </main>
+</template>
+
+<style>
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
